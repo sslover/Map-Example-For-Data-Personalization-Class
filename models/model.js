39a8252b5608b83d06f840cb5b510375f1f843ec
sslover <samjslover@gmail.com>
2015-08-30 21:43:54 -0400

    init commit

---
+++ models/model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// See http://mongoosejs.com/docs/schematypes.html
+
+var personSchema = new Schema({
+	name: String,
+	location: {
+		geo: { type: [Number], index: { type: '2dsphere', sparse: true } },
+		name: String
+	}
+	vacation: {
+		geo: { type: [Number], index: { type: '2dsphere', sparse: true } },
+		name: String
+	}
+	dateAdded : { type: Date, default: Date.now },
+})
+
+// export 'Person' model so we can interact with it in other files
+module.exports = mongoose.model('Person',personSchema);
